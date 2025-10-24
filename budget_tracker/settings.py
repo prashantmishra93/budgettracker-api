48aafc2423ec8a52f13f4b7e97d3f3002932b1bd
Prashant Mishra <84780159+prashantmishra93@users.noreply.github.com>
2025-10-24 10:05:51 +0530

    Update settings.py
    local app server commented

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",   # React dev server
+    # "http://localhost:3000",   # React dev server
     # "http://localhost:3001",   # React dev server
     "https://budgettracker-app-6wpg.onrender.com"
 ]
