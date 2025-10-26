73813fb1d52c4775e158bf9ede41b5a2fc99ce3d
prashantmishra93 <prashantmishra.rcplko@gmail.com>
2025-10-26 10:30:12 +0530

    budget paginated data sorting

--- tracker/views/getBudgetView.py
+++ tracker/views/getBudgetView.py
@@ -4,17 +4,40 @@ from rest_framework.permissions import IsAuthenticated
 from ..models import MonthlyBudget
 from ..serializers import MonthlyBudgetSerializer
 from budget_tracker.utility import Utility
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from math import ceil
 
 # Get all budgets
 class GetBudgetsView(APIView):
     permission_classes = [IsAuthenticated]
     
     def post(self, request):
-        budgets = MonthlyBudget.objects.all().order_by('-year', '-month')
-        serializer = MonthlyBudgetSerializer(budgets, many=True)
+        perPage = int(request.query_params.get('per_page', 10))
+        page = int(request.query_params.get('page', 1))
+
+        qs = MonthlyBudget.objects.filter(user=request.user).order_by('-id')
+
+        paginator = Paginator(qs, perPage)
+        try:
+            entries = paginator.page(page)
+        except PageNotAnInteger:
+            entries = paginator.page(1)
+        except EmptyPage:
+            entries = paginator.page(paginator.num_pages)
+
+        serializer = MonthlyBudgetSerializer(entries, many=True)
+        last_page = ceil(qs.count() / perPage)
+        filterData = {
+            "entries": serializer.data,
+            "total_items": paginator.count,
+            "total_pages": paginator.num_pages,
+            "current_page": entries.number,
+            "per_page": perPage,
+            "last_page": last_page,
+        }
         return Utility.returnFormat(
             message_type='success_msg',
-            data=serializer.data,
-            query='fetch_query',
-            http_status_code=status.HTTP_200_OK
+            data=filterData,
+            query='store_query',
+            http_status_code=status.HTTP_201_CREATED
         )
